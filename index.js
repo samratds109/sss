c57ebb40278e0d481611edb0694d98d304b21d4a
SAMRAT D S <134962353+samratds109@users.noreply.github.com>
2024-10-15 13:44:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
 });
